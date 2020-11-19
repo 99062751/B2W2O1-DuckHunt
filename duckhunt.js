80029d2d63d916f3a38062b21f95548d2c035c60
99062751 <70890704+99062751@users.noreply.github.com>
2020-11-19 10:58:35 +0100

    update

---
+++ duckhunt.js
@@ -0,0 +1,47 @@
+function fly(direction){
+    direction= "N", "NE", "E", "SE", "S", "SW", "W", "NW";
+
+    switch(direction){
+        case 1:
+        direction == "N";
+        fly(direction);
+        break
+
+        case 2:
+        direction == "NE";
+        fly(direction);
+        break
+
+        case 3:
+        direction == "E";
+        fly(direction);
+        break
+
+        case 4:
+        direction == "SE";
+        fly(direction);
+        break
+
+        case 5:
+        direction == "S";
+        fly(direction);
+        break
+
+        case 6:
+        direction == "SW";
+        fly(direction);
+        break
+
+        case 7:
+        direction == "W";
+        fly(direction);
+        break
+
+        case 7:
+        direction == "NW";
+        fly(direction);
+        break
+    }
+
+}
+
