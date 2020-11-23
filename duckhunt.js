fb8c387a526ca13eed4218202e3d1bc565d6f745
99062751 <70890704+99062751@users.noreply.github.com>
2020-11-23 08:54:30 +0100

    update

--- duckhunt.js
+++ duckhunt.js
@@ -1,46 +1,94 @@
+var stage = document.getElementById("container");
+var duck = document.getElementById("duck");
+
+var stage_w = parseInt(window.getComputedStyle(stage).width.replace('px', '')); 
+var stage_h = parseInt(window.getComputedStyle(stage).height.replace('px', '')); 
+
+var eend_w = parseInt(window.getComputedStyle(duck).width.replace('px', '')); 
+var eend_h = parseInt(window.getComputedStyle(duck).height.replace('px', '')); 
+
+var middenpositieX =  stage_w / 2 -  eend_w / 2;
+var middenpositieY =  stage_h / 2 - eend_h / 2;
+
+var game = false;
+
+var directions = ["N", "NE", "E", "SE", "S", "SW", "W", "NW"];
+var direction= directions[Math.floor(Math.random() * 8)];
+
+var button = document.getElementById("start");
+
+button.onclick = start;
+
+
+function start(){
+    game = true;
+    duck.style.top= middenpositieY + "px"; 
+    duck.style.left= middenpositieX + "px";
+    while(game = true){
+        direction= directions[Math.floor(Math.random() * 8)];
+        window.setInterval((fly, 500))
+    }
+}
+
+
 function fly(direction){
-    direction= "N", "NE", "E", "SE", "S", "SW", "W", "NW";
-
+    
     switch(direction){
-        case 1:
-        direction == "N";
-        fly(direction);
-        break
+        case "N":
+            middenpositieX= middenpositieX;
+            middenpositieY= middenpositieY + 75;
+            duck.style.top= middenpositieY+ "px"; 
+            duck.style.left= middenpositieX + "px"; 
+        break;
 
-        case 2:
-        direction == "NE";
-        fly(direction);
-        break
+        case "NE":
+            middenpositieX= middenpositieX + 75;
+            middenpositieY= middenpositieY + 75;
+            duck.style.top= middenpositieY+ "px"; 
+            duck.style.left= middenpositieX + "px"; 
+        break;
 
-        case 3:
-        direction == "E";
-        fly(direction);
-        break
+        case "E":
+            middenpositieX= middenpositieX + 75;
+            middenpositieY= middenpositieY;
+            duck.style.top= middenpositieY+ "px"; 
+            duck.style.left= middenpositieX + "px"; 
+        break;
 
-        case 4:
-        direction == "SE";
-        fly(direction);
-        break
+        case "SE":
+            middenpositieX= middenpositieX + 75;
+            middenpositieY= middenpositieY - 75;
+            duck.style.top= middenpositieY+ "px"; 
+            duck.style.left= middenpositieX + "px"; 
+        break;
 
-        case 5:
-        direction == "S";
-        fly(direction);
-        break
+        case "S":
+            middenpositieX= middenpositieX ;
+            middenpositieY= middenpositieY - 75;
+            duck.style.top= middenpositieY+ "px"; 
+            duck.style.left= middenpositieX + "px"; 
+        break;
 
-        case 6:
-        direction == "SW";
-        fly(direction);
-        break
+        case "SW":
+            middenpositieX= middenpositieX - 75;
+            middenpositieY= middenpositieY - 75;
+            duck.style.top= middenpositieY+ "px"; 
+            duck.style.left= middenpositieX + "px"; 
+        break;
 
-        case 7:
-        direction == "W";
-        fly(direction);
-        break
+        case "W":
+            middenpositieX= middenpositieX;
+            middenpositieY= middenpositieY + 75;
+            duck.style.top= middenpositieY+ "px"; 
+            duck.style.left= middenpositieX + "px"; 
+        break;
 
-        case 7:
-        direction == "NW";
-        fly(direction);
-        break
+        case "NW":
+            middenpositieX= middenpositieX + 75;
+            middenpositieY= middenpositieY + 75;
+            duck.style.top= middenpositieY+ "px"; 
+            duck.style.left= middenpositieX + "px"; 
+        break;
     }
 
 }
